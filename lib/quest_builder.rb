33f8434ecbf73449ad2a44bbee8d5b3d8d3d177b
Vincent <vincent.jumeaux@gmail.com>
2016-02-25 17:56:55 +0100

    add validations on quest

--- lib/quest_builder.rb
+++ lib/quest_builder.rb
@@ -1,10 +1,12 @@
 require 'pry'
+
 module Modiz
   class QuestBuilder
     attr_reader :to_hash
 
     def initialize(quest_lines)
       @lines = quest_lines
+      validations
     end
 
     def to_hash
@@ -14,7 +16,6 @@ module Modiz
     end
 
     private
-
     def title
       @lines.first.tr("#", "").strip
     end
@@ -29,6 +30,20 @@ module Modiz
 
     def goal_index
       @lines.index {|s| s.include?("## Objectifs")}
+    end
+
+    def validations
+      if title.empty?
+        raise InvalidQuest, "Le fichier n'a pas de titre de quête. Ce doit être la première ligne du fichier et commencer par '# '"
+      end
+
+      unless goal_index
+        raise InvalidQuest, "Le fichier n'a pas d'objectif. La Quête doit contenir une liste objectifs avant la section Etapes "
+      end
+
+      if description.empty?
+        raise InvalidQuest, "Le fichier n'a pas de description de quêtes. Donnez un peu de contexte tout de même !"
+      end
     end
   end
 end
--- test/errors_test.rb
+++ test/errors_test.rb
@@ -9,6 +9,24 @@ module Modiz
       assert_match /Le fichier doit contenir ## Etapes et ## Challenge/, err.message
     end
 
+    def test_no_quest_title
+      quest_file = "\n\n## Etapes\n\n## Challenge\n\n"
+      err = raise_error quest_file
+      assert_match /Le fichier n'a pas de titre de quête./, err.message
+    end
+
+    def test_no_quest_objectif
+      quest_file = "# Apprendre le Markdown\n\n## Etapes\n\n## Challenge\n\n"
+      err = raise_error quest_file
+      assert_match /Le fichier n'a pas d'objectif./, err.message
+    end
+
+    def test_no_quest_description
+      quest_file = "# Apprendre le Markdown\n\n## Objectifs\n\n## Etapes\n\n## Challenge\n\n"
+      err = raise_error quest_file
+      assert_match /Le fichier n'a pas de description de quêtes./, err.message
+    end
+
     def raise_error quest_file
       assert_raises InvalidQuest do
         Parser.run(quest_file)
